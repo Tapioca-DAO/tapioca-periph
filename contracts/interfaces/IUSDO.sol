a665190a1ec62d61f8f0509c6026029e4efd2e3b
cryptotechmaker <grigore.cosmin@gmail.com>
2023-05-01 15:27:00 +0300

    Added IUSDOBase

--- contracts/interfaces/IUSDO.sol
+++ contracts/interfaces/IUSDO.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-interface IUSDO is IERC20Metadata {
+interface IUSDOBase {
     function mint(address _to, uint256 _amount) external;
 
     function burn(address _from, uint256 _amount) external;
 }
+
+interface IUSDO is IUSDOBase, IERC20Metadata {}
