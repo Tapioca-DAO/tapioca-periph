bb02cab078d10b7091bd9b1c824436d3db68b57c
cryptotechmaker <grigore.cosmin@gmail.com>
2023-06-15 10:09:53 +0300

    .

--- contracts/Magnetar/modules/MagnetarMarketModule.sol
+++ contracts/Magnetar/modules/MagnetarMarketModule.sol
@@ -534,7 +534,7 @@ contract MagnetarMarketModule is MagnetarV2Storage {
         {} catch {
             return;
         }
-        
+
         yieldBox.withdraw(assetId, from, address(this), amount, 0);
         bytes memory _adapterParams;
         ISendFrom.LzCallParams memory callParams = ISendFrom.LzCallParams({
