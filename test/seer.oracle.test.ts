bcdcf8d29664c0b76f98a938124f6dd6635257a5
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-06-17 13:11:22 -0400

    patch: Updated oracle tests

--- test/seer.oracle.test.ts
+++ test/seer.oracle.test.ts
@@ -1,9 +1,19 @@
-import hre, { ethers } from 'hardhat';
-import { expect } from 'chai';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import { BN, register, registerFork } from './test.utils';
+import hre from 'hardhat';
+import { register } from './test.utils';
+
+let runTestMainnet: any = (fn: () => any) =>
+    describe('Seer only mainnet/arb fork', fn);
+if (hre.network.config.chainId !== 1)
+    runTestMainnet = (fn: () => any) => describe.skip('Seer mainnet', fn);
+
+let runTestArb: any = (fn: () => any) =>
+    describe.skip('Seer only mainnet/arb fork', fn);
+if (hre.network.config.chainId !== 42161)
+    runTestArb = (fn: () => any) => describe.skip('Seer arbitrum', fn);
+
 // Tests are expected to be done on forked mainnet
-describe.only('Seer', () => {
+runTestMainnet(() => {
     it('DAI/USDC', async () => {
         const { deployer } = await loadFixture(register);
 
@@ -12,6 +22,7 @@ describe.only('Seer', () => {
         ).deploy(
             'DAI/USDC', // Name
             'DAI/USDC', // Symbol
+            18, // Decimals
             [
                 '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // DAI
                 '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
@@ -34,7 +45,10 @@ describe.only('Seer', () => {
         );
 
         console.log(
-            hre.ethers.utils.formatEther((await seer.peek('0x00')).rate),
+            hre.ethers.utils.formatUnits(
+                (await seer.peek('0x00')).rate,
+                await seer.decimals(),
+            ),
         );
     });
 
@@ -46,6 +60,7 @@ describe.only('Seer', () => {
         ).deploy(
             'ETH/USDC', // Name
             'ETH/USDC', // Symbol
+            18, // Decimals
             [
                 '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // ETH
                 '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
@@ -68,7 +83,53 @@ describe.only('Seer', () => {
         );
 
         console.log(
-            hre.ethers.utils.formatEther((await seer.peek('0x00')).rate),
+            hre.ethers.utils.formatUnits(
+                (await seer.peek('0x00')).rate,
+                await seer.decimals(),
+            ),
+        );
+    });
+
+    it('TriCrypto', async () => {
+        const { deployer } = await loadFixture(register);
+
+        const seer = await (
+            await hre.ethers.getContractFactory('ARBTriCryptoOracle')
+        ).deploy(
+            'TriCrypto', // Name
+            'TriCrypto', // Symbol
+            '0xD51a44d3FaE010294C616388b506AcdA1bfAAE46', // TriCrypto
+            '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c', // BTC feed
+            '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419', // ETH feed
+            '0x3E7d1eAB13ad0104d2750B8863b489D65364e32D', // USDT feed
+            '0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23', // WBTC feed
+        );
+
+        console.log(
+            hre.ethers.utils.formatUnits(
+                (await seer.peek('0x00')).rate,
+                await seer.decimals(),
+            ),
+        );
+    });
+});
+
+runTestArb(() => {
+    it('GLP', async () => {
+        const { deployer } = await loadFixture(register);
+
+        const seer = await (
+            await hre.ethers.getContractFactory('GLPOracle')
+        ).deploy(
+            '0x3963FfC9dff443c2A94f21b129D429891E32ec18', // GLP Manager
+        );
+
+        console.log((await seer.peek('0x00')).rate);
+        console.log(
+            hre.ethers.utils.formatUnits(
+                (await seer.peek('0x00')).rate,
+                await seer.decimals(),
+            ),
         );
     });
 });
