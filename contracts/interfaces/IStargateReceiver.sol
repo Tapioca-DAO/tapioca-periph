34a2f9004bc4730708824535b45487495845ae4c
cryptotechmaker <grigore.cosmin@gmail.com>
2023-08-09 12:11:53 +0300

    added IStargateReceiver interface

---
+++ contracts/interfaces/IStargateReceiver.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+interface IStargateReceiver {
+    function sgReceive(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint256 _nonce,
+        address _token,
+        uint256 amountLD,
+        bytes memory payload
+    ) external;
+}
