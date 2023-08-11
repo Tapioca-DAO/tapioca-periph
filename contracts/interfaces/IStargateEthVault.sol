4d4cf3ffb215adc8b9ecdc83304a74fe70804499
cryptotechmaker <grigore.cosmin@gmail.com>
2023-08-11 09:47:39 +0300

    updated IStargateEthVault

--- contracts/interfaces/IStargateEthVault.sol
+++ contracts/interfaces/IStargateEthVault.sol
@@ -5,4 +5,12 @@ interface IStargateEthVault {
     function deposit() external payable;
 
     function withdraw(uint wad) external;
+
+    function balanceOf(address) external view returns (uint256);
+
+    function approve(address guy, uint wad) external returns (bool);
+
+    function transfer(address dst, uint wad) external returns (bool);
+
+    function transferFrom(address src, address dst, uint wad) external returns (bool);
 }
