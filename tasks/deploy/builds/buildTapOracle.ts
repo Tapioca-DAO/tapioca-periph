7b8f3397d90989c89b31d93e01156440ee3817c4
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-10-30 18:39:14 -0400

    feat: New task to deploy `GLP` & `TAP` oracles

---
+++ tasks/deploy/builds/buildTapOracle.ts
@@ -0,0 +1,52 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { IDeployerVMAdd } from 'tapioca-sdk/dist/ethers/hardhat/DeployerVM';
+import { Seer__factory } from '../../../typechain';
+import { displaySeerArgs, nonNullValues } from '../../utils';
+import { ARGS_CONFIG } from '../config';
+
+export const buildTapOracle = async (
+    hre: HardhatRuntimeEnvironment,
+): Promise<IDeployerVMAdd<Seer__factory>> => {
+    console.log('[+] buildTAPOracle');
+
+    const chainID = await hre.getChainId();
+    if (chainID !== hre.SDK.config.EChainID.ARBITRUM) {
+        throw '[-] TAP Oracle only available on Arbitrum';
+    }
+    const deployer = (await hre.ethers.getSigners())[0];
+
+    const args: Parameters<Seer__factory['deploy']> = [
+        'TAP/USDC', // Name
+        'TAP/USDC', // Symbol
+        18, // Decimals
+        [
+            ARGS_CONFIG[chainID].TAP_ORACLE.TAP_ADDRESS, // TAP
+            ARGS_CONFIG[chainID].MISC.USDC_ADDRESS, // USDC
+        ],
+        [
+            ARGS_CONFIG[chainID].TAP_ORACLE.TAP_USDC_LP_ADDRESS, /// LP TAP/USDC
+        ],
+        [1], // Multiply/divide Uni
+        600, // TWAP, 5min
+        10, // Observation length that each Uni pool should have
+        0, // Whether we need to use the last Chainlink oracle to convert to another
+        // CL path
+        [],
+        [], // Multiply/divide CL
+        86400, // CL period before stale, 1 day
+        [deployer.address], // Owner
+        hre.ethers.utils.formatBytes32String('TAP/USDC'), // Description,
+        ARGS_CONFIG[chainID].MISC.CL_SEQUENCER, // CL Sequencer
+        deployer.address, // Owner
+    ];
+
+    // Check for null values
+    displaySeerArgs(args);
+    nonNullValues(args);
+
+    return {
+        contract: await hre.ethers.getContractFactory('Seer'),
+        deploymentName: 'TapOracle',
+        args,
+    };
+};
