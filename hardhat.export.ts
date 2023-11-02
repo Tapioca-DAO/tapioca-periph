463a930e22ccfa5af86a69d293402b2a132f200b
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-11-02 18:40:27 -0400

    chore: Updated fork to use Mainnet

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -65,14 +65,14 @@ const config: HardhatUserConfig & { dodoc?: any; typechain?: any } = {
     networks: {
         hardhat: {
             saveDeployments: false,
-            // chainId: 1,
-            chainId: 42161,
+            chainId: 1,
+            // chainId: 42161,
             mining: { auto: true },
             forking: {
-                // url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-                url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-                // blockNumber: 17068626, // Mainnet
-                blockNumber: 145526897, // Arb
+                url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+                // url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+                blockNumber: 17068626, // Mainnet
+                // blockNumber: 145526897, // Arb
             },
             hardfork: 'merge',
             allowUnlimitedContractSize: true,
