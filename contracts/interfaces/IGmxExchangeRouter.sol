9011d3d1a45b74c921972329dce74788066e6635
cryptotechmaker <grigore.cosmin@gmail.com>
2023-11-02 12:57:48 +0200

    periph artifacts

---
+++ contracts/interfaces/IGmxExchangeRouter.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+interface IGmxExchangeRouter {
+    struct CreateDepositParams {
+        address receiver;
+        address callbackContract;
+        address uiFeeReceiver;
+        address market;
+        address initialLongToken;
+        address initialShortToken;
+        address[] longTokenSwapPath;
+        address[] shortTokenSwapPath;
+        uint256 minMarketTokens;
+        bool shouldUnwrapNativeToken;
+        uint256 executionFee;
+        uint256 callbackGasLimit;
+    }
+    struct CreateWithdrawalParams {
+        address receiver;
+        address callbackContract;
+        address uiFeeReceiver;
+        address market;
+        address[] longTokenSwapPath;
+        address[] shortTokenSwapPath;
+        uint256 minLongTokenAmount;
+        uint256 minShortTokenAmount;
+        bool shouldUnwrapNativeToken;
+        uint256 executionFee;
+        uint256 callbackGasLimit;
+    }
+
+    function sendWnt(address receiver, uint256 amount) external payable;
+
+    function sendTokens(
+        address token,
+        address receiver,
+        uint256 amount
+    ) external payable;
+
+    function createDeposit(
+        CreateDepositParams calldata params
+    ) external payable returns (bytes32);
+
+    function createWithdrawal(
+        CreateWithdrawalParams calldata params
+    ) external payable returns (bytes32);
+
+    function multicall(
+        bytes[] calldata data
+    ) external payable returns (bytes[] memory results);
+}
