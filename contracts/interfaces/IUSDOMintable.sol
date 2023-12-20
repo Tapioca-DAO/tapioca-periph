5f4f4d93ab1f9a94fdb77796734db3ddf846d088
cryptotechmaker <grigore.cosmin@gmail.com>
2023-12-20 12:44:48 +0200

    CU-86dqt9q1p: added IUSDOMintable interface

---
+++ contracts/interfaces/IUSDOMintable.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+interface IUSDOMintable {
+    function mint(address _to, uint256 _amount) external;
+
+    function burn(address _from, uint256 _amount) external;
+}
