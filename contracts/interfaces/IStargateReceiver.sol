2271d8534b03939f7e1492d3785e8c9b702bc181
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-01-02 21:13:55 -0500

    patch: add `payable` to `sgReceive` interface

--- contracts/interfaces/IStargateReceiver.sol
+++ contracts/interfaces/IStargateReceiver.sol
@@ -2,12 +2,12 @@
 pragma solidity 0.8.19;
 
 interface IStargateReceiver {
-    function sgReceive(
-        uint16 _srcChainId,
-        bytes memory _srcAddress,
-        uint256 _nonce,
-        address _token,
-        uint256 amountLD,
-        bytes memory payload
-    ) external;
+  function sgReceive(
+    uint16 _srcChainId,
+    bytes memory _srcAddress,
+    uint256 _nonce,
+    address _token,
+    uint256 amountLD,
+    bytes memory payload
+  ) external payable;
 }
