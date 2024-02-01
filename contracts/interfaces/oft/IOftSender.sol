172ac7acbcda762724db4e071e183aa79606b267
cryptotechmaker <grigore.cosmin@gmail.com>
2024-02-01 15:13:45 +0200

    chore: IOftSender interface

---
+++ contracts/interfaces/oft/IOftSender.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+// LZ
+import {
+    MessagingReceipt, OFTReceipt
+} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
+
+// Tapioca
+import {LZSendParam} from "tapioca-periph/interfaces/periph/ITapiocaOmnichainEngine.sol";
+
+interface IOftSender {
+   function sendPacket(LZSendParam calldata _lzSendParam, bytes calldata _composeMsg)
+        external
+        payable
+        returns (MessagingReceipt memory msgReceipt, OFTReceipt memory oftReceipt);
+       
+}
