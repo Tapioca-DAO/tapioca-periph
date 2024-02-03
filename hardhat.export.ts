aaf929b0feb2474a64cc3890db92db3fd1a83a78
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-02-03 12:07:32 -0500

    chore: gen + typechain folder init

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -28,8 +28,10 @@ declare global {
 // Load the env vars from the .env/<network>.env file. the <network> file name is the same as the network in hh `--network arbitrum_sepolia`
 loadEnv();
 // Check if the folder /gen/typechain exists, if not, create it. This is needed if the repo was freshly cloned.
-if (!fs.existsSync('./gen/typechain')) {
-    fs.mkdirSync('./gen/typechain');
+if (!fs.existsSync('./gen/typechain/index.ts')) {
+    try {
+        fs.mkdirSync('./gen/typechain', { recursive: true });
+    } catch (e) {}
     fs.writeFileSync('./gen/typechain/index.ts', '');
 }
 
