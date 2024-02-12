a7e3f101d17dcc7a3d1f25887e977b2953160067
cryptotechmaker <grigore.cosmin@gmail.com>
2024-02-12 23:08:16 +0200

    chore: iweth9 interface

--- contracts/interfaces/external/weth/IWETH9.sol
+++ contracts/interfaces/external/weth/IWETH9.sol
@@ -14,7 +14,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
    
 */
 
-interface IWETH9 is IERC20 {
+interface IWeth9 is IERC20 {
     /// @notice Deposit ether to get wrapped ether
     function deposit() external payable;
 
