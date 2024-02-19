a587b157ecdccfdf22c77a2b504817c7e685fdae
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-02-18 19:29:09 -0500

    feat: Add `1Inch` swapper support

--- contracts/Swapper/ZeroXSwapper.sol
+++ contracts/Swapper/ZeroXSwapper.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.22;
 
 // External
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 // Tapioca
 import {IZeroXSwapper} from "tapioca-periph/interfaces/periph/IZeroXSwapper.sol";
@@ -20,7 +21,7 @@ import {ICluster} from "tapioca-periph/interfaces/periph/ICluster.sol";
 */
 
 /// @title ZeroX swapper
-contract ZeroXSwapper is IZeroXSwapper {
+contract ZeroXSwapper is IZeroXSwapper, Ownable {
     using SafeERC20 for IERC20;
 
     /// ************
@@ -28,7 +29,10 @@ contract ZeroXSwapper is IZeroXSwapper {
     /// ************
 
     address public zeroXProxy;
+    address public oneInchProxy;
     ICluster public cluster;
+
+    bool public isOneInchEnabled;
 
     /// **************
     /// *** ERRORS ***
@@ -40,10 +44,11 @@ contract ZeroXSwapper is IZeroXSwapper {
     error MinSwapFailed(uint256 amountOut);
     error SenderNotValid(address sender);
 
-    constructor(address _zeroXProxy, ICluster _cluster) {
+    constructor(address _zeroXProxy, ICluster _cluster, address _owner) {
         if (_zeroXProxy == address(0)) revert ZeroAddress();
         zeroXProxy = _zeroXProxy;
         cluster = _cluster;
+        transferOwnership(_owner);
     }
 
     /// **********************
@@ -52,7 +57,7 @@ contract ZeroXSwapper is IZeroXSwapper {
 
     /// @notice swaps a token for another, using 0x as a swap aggregator
     /// @dev All of the parameters below are provided by the API response.
-    /// @param swapData the swap data
+    /// @param swapData the swap data. Either 0x or 1inch
     /// @param amountIn the amount of sellToken to sell
     /// @param minAmountOut the minimum amount of buyToken bought
     /// @return amountOut the amount of buyToken bought
@@ -63,10 +68,14 @@ contract ZeroXSwapper is IZeroXSwapper {
     {
         if (!cluster.isWhitelisted(0, msg.sender)) revert SenderNotValid(msg.sender);
 
-        if (swapData.swapTarget != zeroXProxy) revert InvalidProxy(swapData.swapTarget, zeroXProxy);
-
         // Transfer tokens to this contract
         swapData.sellToken.safeTransferFrom(msg.sender, address(this), amountIn);
+
+        if (!isOneInchEnabled && swapData.swapTarget != zeroXProxy) {
+            revert InvalidProxy(swapData.swapTarget, zeroXProxy);
+        } else if (swapData.swapTarget != oneInchProxy) {
+            revert InvalidProxy(swapData.swapTarget, oneInchProxy);
+        }
 
         // Approve the 0x proxy to spend the sell token, and call the swap function
         swapData.sellToken.safeApprove(swapData.swapTarget, amountIn);
@@ -80,6 +89,11 @@ contract ZeroXSwapper is IZeroXSwapper {
 
         // Transfer the bought tokens to the sender
         swapData.buyToken.safeTransfer(msg.sender, amountOut);
+    }
+
+    /// @notice updates the swap function to use 1inch instead of 0x.
+    function setIsOneInchEnabled(bool _isOneInchEnabled) external onlyOwner {
+        isOneInchEnabled = _isOneInchEnabled;
     }
 
     /**
