f27bd631da9c5ebe2a179e00e8e0d4c79f6de432
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-03-11 09:29:48 -0400

    chore(`tasks`): Removed util funcs

--- tasks/utils.ts
+++ tasks/utils.ts
@@ -1,6 +1,4 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import _ from 'lodash';
-import { SeerCLSolo__factory, Seer__factory } from '../typechain';
 
 export const loadVM = async (hre: HardhatRuntimeEnvironment, tag?: string) => {
     const VM = new hre.SDK.DeployerVM(hre, {
@@ -11,49 +9,4 @@ export const loadVM = async (hre: HardhatRuntimeEnvironment, tag?: string) => {
         tag,
     });
     return VM;
-};
-
-export const nonNullValues = <T>(args: Array<any>) => {
-    for (let i = 0; i < args.length; i++) {
-        if (_.isNil(args[i])) {
-            throw `[-] Argument ${i} is null`;
-        }
-    }
-};
-
-export const displaySeerArgs = (args: Parameters<Seer__factory['deploy']>) => {
-    let i = 0;
-    console.log('[+] With args:');
-    console.log(`\t[${i}]Name:`, args[i++]);
-    console.log(`\t[${i}]Symbol:`, args[i++]);
-    console.log(`\t[${i}]Decimal:`, args[i++]);
-    console.log(`\t[${i}]Addresses:`, args[i++]);
-    console.log(`\t[${i}]LP:`, args[i++]);
-    console.log(`\t[${i}]Multiply/divide Uni:`, args[i++]);
-    console.log(`\t[${i}]TWAP:`, args[i++]);
-    console.log(`\t[${i}]Uni min observation length:`, args[i++]);
-    console.log(`\t[${i}]ChainLink usage:`, args[i++]);
-    console.log(`\t[${i}]ChainLink path:`, args[i++]);
-    console.log(`\t[${i}]Multiply/Divide ChainLink:`, args[i++]);
-    console.log(`\t[${i}]ChainLink stale period(in seconds):`, args[i++]);
-    console.log(`\t[${i}]Guardians:`, args[i++]);
-    console.log(`\t[${i}]Description:`, args[i++]);
-    console.log(`\t[${i}]ChainLink sequencer:`, args[i++]);
-    console.log(`\t[${i}]Admins:`, args[i++]);
-};
-
-export const displaySeerCLSoloArgs = (
-    args: Parameters<SeerCLSolo__factory['deploy']>,
-) => {
-    let i = 0;
-    console.log('[+] With args:');
-    console.log(`\t[${i}]Name:`, args[i++]);
-    console.log(`\t[${i}]Symbol:`, args[i++]);
-    console.log(`\t[${i}]Decimal:`, args[i++]);
-    console.log(`\t[${i}]LP:`, args[i++]);
-    console.log(`\t[${i}]Multiply/divide CL:`, args[i++]);
-    console.log(`\t[${i}]Guardians:`, args[i++]);
-    console.log(`\t[${i}]Description:`, args[i++]);
-    console.log(`\t[${i}]ChainLink sequencer:`, args[i++]);
-    console.log(`\t[${i}]Admins:`, args[i++]);
 };
