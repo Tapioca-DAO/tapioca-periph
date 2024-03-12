8d4e2b66d548e118c12c96932a6f5676df187f89
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-03-12 13:34:19 -0400

    patch(`tasks`): Using SDK `TAP_TASK` wrapper

--- tasks/scopes/deployScope.ts
+++ tasks/scopes/deployScope.ts
@@ -2,49 +2,24 @@ import { scope } from 'hardhat/config';
 import { deployPreLbpStack__task } from 'tasks/deploy/1-deployPreLbpStack';
 import { deployPostLbpStack__task } from 'tasks/deploy/2-deployPostLbpStack';
 import { deployERC20Mock__task } from 'tasks/deploy/mock/deployERC20Mock';
+import { TAP_TASK } from 'tapioca-sdk';
 
 const deployScope = scope('deploys', 'Deployment tasks');
 
-deployScope
-    .task(
+TAP_TASK(
+    deployScope.task(
         'preLbp',
         'Deploy Cluster, Pearlmit and Magnetar',
         deployPreLbpStack__task,
-    )
-    .addOptionalParam(
-        'tag',
-        'The tag to use for the deployment. Defaults to "default" if not specified.',
-        'default',
-    )
-    .addFlag(
-        'load',
-        'Load the contracts from the database instead of building them.',
-    )
-    .addFlag('verify', 'Add to verify the contracts after deployment.');
+    ),
+);
 
-deployScope
-    .task('postLbp', 'Deploy Oracles', deployPostLbpStack__task)
-    .addOptionalParam(
-        'tag',
-        'The tag to use for the deployment. Defaults to "default" if not specified.',
-        'default',
-    )
-    .addFlag(
-        'load',
-        'Load the contracts from the database instead of building them.',
-    )
-    .addFlag('verify', 'Add to verify the contracts after deployment.');
+TAP_TASK(
+    deployScope.task('postLbp', 'Deploy Oracles', deployPostLbpStack__task),
+);
 
-deployScope
-    .task('erc20mock', 'Deploy an ERC20 Mock', deployERC20Mock__task)
-    .addParam('name', 'The name of the ERC20 token to deploy.')
-    .addOptionalParam(
-        'tag',
-        'The tag to use for the deployment. Defaults to "default" if not specified.',
-        'default',
-    )
-    .addFlag(
-        'load',
-        'Load the contracts from the database instead of building them.',
-    )
-    .addFlag('verify', 'Add to verify the contracts after deployment.');
+TAP_TASK(
+    deployScope
+        .task('erc20mock', 'Deploy an ERC20 Mock', deployERC20Mock__task)
+        .addParam('name', 'The name of the ERC20 token to deploy.'),
+);
