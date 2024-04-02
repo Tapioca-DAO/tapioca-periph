d008bca22d9582539d02bfaefa59463f03da10f7
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-04-02 14:54:58 -0400

    feat: New `SDaiOracle`

---
+++ contracts/oracle/implementation/ethereum/sDAIOracle.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+// External
+
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+// Tapioca
+import {AccessControlDefaultAdminRules} from "../../external/AccessControlDefaultAdminRules.sol";
+import {AggregatorInterface} from "tapioca-periph/interfaces/external/chainlink/AggregatorInterface.sol";
+import {ITapiocaOracle} from "tapioca-periph/interfaces/periph/ITapiocaOracle.sol";
+import {SequencerCheck} from "../../utils/SequencerCheck.sol";
+
+contract SDaiOracle is ITapiocaOracle, SequencerCheck, AccessControlDefaultAdminRules, ReentrancyGuard {
+    AggregatorInterface private immutable sDaiOracle;
+
+    constructor(AggregatorInterface _sDaiOracle, address _sequencerUptimeFeed, address _admin)
+        SequencerCheck(_sequencerUptimeFeed)
+        AccessControlDefaultAdminRules(3 days, _admin)
+    {
+        sDaiOracle = _sDaiOracle;
+
+        _grantRole(SEQUENCER_ROLE, _admin);
+    }
+
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    function _get() internal view returns (uint256) {
+        return sDaiOracle.latestAnswer(true) * 1e10; // Chainlink returns 8 decimals, we need 18
+    }
+
+    // Get the latest exchange rate
+    /// @inheritdoc ITapiocaOracle
+    function get(bytes calldata) public override nonReentrant returns (bool success, uint256 rate) {
+        _sequencerBeatCheck();
+        return (true, _get());
+    }
+
+    // Check the last exchange rate without any state changes
+    /// @inheritdoc ITapiocaOracle
+    function peek(bytes calldata) public view override returns (bool success, uint256 rate) {
+        return (true, _get());
+    }
+
+    // Check the current spot exchange rate without any state changes
+    /// @inheritdoc ITapiocaOracle
+    function peekSpot(bytes calldata data) external view override returns (uint256 rate) {
+        (, rate) = peek(data);
+    }
+
+    /// @inheritdoc ITapiocaOracle
+    function name(bytes calldata) public pure override returns (string memory) {
+        return "sDAI/USD";
+    }
+
+    /// @inheritdoc ITapiocaOracle
+    function symbol(bytes calldata) public pure override returns (string memory) {
+        return "sDAI/USD";
+    }
+
+    /// @notice Changes the grace period for the sequencer update
+    /// @param _gracePeriod New stale period (in seconds)
+    function changeGracePeriod(uint32 _gracePeriod) external override onlyRole(SEQUENCER_ROLE) {
+        GRACE_PERIOD_TIME = _gracePeriod;
+    }
+}
