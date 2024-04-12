655ebc119f4d72f4927f58fa1218f61dd2516a7d
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-04-11 21:09:50 -0400

    patch(`Magnetar`): revert if rescue eth fail on burst

--- contracts/Magnetar/Magnetar.sol
+++ contracts/Magnetar/Magnetar.sol
@@ -174,7 +174,8 @@ contract Magnetar is BaseMagnetar {
         // @dev Magnetar shouldn't hold eth so this is safe
         // @dev if allowFailure is `true` and action fails, ETH might be left here
         if (address(this).balance > 0) {
-            msg.sender.call{value: address(this).balance}("");
+            (bool success,) = msg.sender.call{value: address(this).balance}("");
+            if (!success) revert Magnetar_FailRescueEth();
         }
     }
 
