f9e8af4521be5567bad149305d27e9ef4e9e3ecc
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-04-17 23:14:07 -0400

    patch: `IMarket`

--- contracts/interfaces/bar/IMarket.sol
+++ contracts/interfaces/bar/IMarket.sol
@@ -23,46 +23,47 @@ enum Module {
 }
 
 interface IMarket {
-    function asset() external view returns (address);
+    function _asset() external view returns (address);
 
-    function assetId() external view returns (uint256);
+    function _assetId() external view returns (uint256);
 
-    function collateral() external view returns (address);
+    function _collateral() external view returns (address);
 
-    function collateralId() external view returns (uint256);
+    function _collateralId() external view returns (uint256);
 
-    function totalBorrowCap() external view returns (uint256);
+    function _totalBorrowCap() external view returns (uint256);
 
-    function totalCollateralShare() external view returns (uint256);
+    function _totalCollateralShare() external view returns (uint256);
 
-    function userBorrowPart(address) external view returns (uint256);
+    function _userBorrowPart(address) external view returns (uint256);
 
-    function userCollateralShare(address) external view returns (uint256);
+    function _userCollateralShare(address) external view returns (uint256);
 
-    function totalBorrow() external view returns (uint128 elastic, uint128 base);
+    function _totalBorrow() external view returns (uint128 elastic, uint128 base);
 
-    function oracle() external view returns (address);
+    function _oracle() external view returns (address);
 
-    function oracleData() external view returns (bytes memory);
+    function _oracleData() external view returns (bytes memory);
 
-    function exchangeRate() external view returns (uint256);
+    function _exchangeRate() external view returns (uint256);
+
+    function _liquidationMultiplier() external view returns (uint256);
+
+    function _penrose() external view returns (address);
+
+    function _collateralizationRate() external view returns (uint256);
+
+    function _liquidationBonusAmount() external view returns (uint256);
+
+    function _liquidationCollateralizationRate() external view returns (uint256);
 
     function yieldBox() external view returns (address payable);
 
-    function liquidationMultiplier() external view returns (uint256);
-
     function refreshPenroseFees() external returns (uint256 feeShares);
 
-    function penrose() external view returns (address);
-
     function owner() external view returns (address);
-
-    function collateralizationRate() external view returns (uint256);
 
     function execute(Module[] calldata modules, bytes[] calldata calls, bool revertOnFail)
         external
         returns (bool[] memory successes, bytes[] memory results);
-
-    function liquidationBonusAmount() external view returns (uint256);
-    function liquidationCollateralizationRate() external view returns (uint256);
 }
