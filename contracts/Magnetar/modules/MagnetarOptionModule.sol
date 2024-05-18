937e56d09d689ce7b180a89cd1b607ae36696b8b
cryptotechmaker <grigore.cosmin@gmail.com>
2024-05-18 16:03:28 +0300

    patch(`tOLP unlock params`): Update `MagnetarOptionModule._unlock` with latest tOLP params [`86dtjd3ku`]
    - `user` param doesn't exist anymore in `tOLP` -> updated the interface and _unlock method code
    - in case owner of `tOLPId` is `Magnetar`, perform a `yieldBox.transfer(sglAssetId, ybShares)` back to `data.user`

--- contracts/Magnetar/modules/MagnetarOptionModule.sol
+++ contracts/Magnetar/modules/MagnetarOptionModule.sol
@@ -339,8 +339,17 @@ contract MagnetarOptionModule is MagnetarBaseModule {
         address ownerOfTOLP = IERC721(data.removeAndRepayData.unlockData.target).ownerOf(tOLPId);
         if (ownerOfTOLP != data.user && ownerOfTOLP != address(this)) revert Magnetar_ActionParamsMismatch();
 
+        (uint128 sglAssetId, uint128 ybShares,,) = ITapiocaOptionLiquidityProvision(data.removeAndRepayData.unlockData.target).lockPositions(tOLPId);
+
+        // will be sent to `data.user` or `address(this)`
         ITapiocaOptionLiquidityProvision(data.removeAndRepayData.unlockData.target).unlock(
-            tOLPId, data.externalData.singularity, data.user
+            tOLPId, data.externalData.singularity
         );
+
+        // transfer unlocked position to the user in case owner is `address(this)`
+        if (ownerOfTOLP == address(this)) {
+            IYieldBox _yieldBox = IYieldBox(ITapiocaOptionLiquidityProvision(data.removeAndRepayData.unlockData.target).yieldBox());
+            _yieldBox.transfer(address(this), data.user, sglAssetId, ybShares);
+        }
     }
 }
