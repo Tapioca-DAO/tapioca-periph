79be814e0703056ddeffc69ce8b201173e2396f1
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-30 12:02:13 -0400

    Revert "patch(`Magnetar`): check `address(this)` whitelist [`86dtnnvpb`]"
    This reverts commit 1760e4392712136714512233b2915f7cb0c51b0d.

--- contracts/Magnetar/Magnetar.sol
+++ contracts/Magnetar/Magnetar.sol
@@ -80,9 +80,6 @@ contract Magnetar is BaseMagnetar, ERC1155Holder {
      * @param calls The list of actions to perform
      */
     function burst(MagnetarCall[] calldata calls) external payable {
-        // @dev: make sure the current magnetar is whitelisted
-        _checkWhitelisted(address(this));
-
         uint256 valAccumulator;
 
         uint256 length = calls.length;
@@ -246,7 +243,7 @@ contract Magnetar is BaseMagnetar, ERC1155Holder {
                 if (isErr) revert Magnetar_PearlmitTransferFailed();
             }
             if (funcSig == ITapiocaOmnichainEngine.sendPacketFrom.selector) {
-                (LZSendParam memory lzSendParam_,) = abi.decode(_actionCalldata[4:], (LZSendParam, bytes));
+                (, LZSendParam memory lzSendParam_,) = abi.decode(_actionCalldata[4:], (address, LZSendParam, bytes));
                 uint256 amount_ = lzSendParam_.sendParam.amountLD;
                 bool isErr = pearlmit.transferFromERC20(msg.sender, address(this), _target, amount_); //sendPacketFrom happens on msg.sender
                 if (isErr) revert Magnetar_PearlmitTransferFailed();
@@ -279,7 +276,7 @@ contract Magnetar is BaseMagnetar, ERC1155Holder {
             selectorValidated = true;
         }
 
-        if (funcSig == ITapiocaOmnichainEngine.sendPacket.selector) {
+        if (funcSig == ITapiocaOmnichainEngine.sendPacketFrom.selector) {
             selectorValidated = true;
             (address from_, LZSendParam memory lzSendParam_,) =
                 abi.decode(_actionCalldata[4:], (address, LZSendParam, bytes));
