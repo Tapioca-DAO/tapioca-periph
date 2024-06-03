3adea61f78f92e55815e8bb3f7abad70494250b0
Razine Bouache <razine.bouache@gmail.com>
2024-06-03 16:02:19 -0400

    add PearlmitHandlerMock to call internal functions

---
+++ test/mocks/PearlmitHandlerMock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {PearlmitHandler} from "../../contracts/pearlmit/PearlmitHandler.sol";
+import {IPearlmit} from "tapioca-periph/interfaces/periph/IPearlmit.sol";
+import {PearlmitHash} from "../../contracts/pearlmit/PearlmitHash.sol";
+
+contract PearlmitHandlerMock is PearlmitHandler {
+    constructor(IPearlmit _pearlmit) PearlmitHandler(_pearlmit) {}
+
+    function isERC721Approved_(address _owner, address spender, address token, uint256 id)
+        external
+        view
+        returns (bool)
+    {
+        bool isApproved = isERC721Approved(_owner, spender, token, id);
+        return (isApproved);
+    }
+
+    function isERC20Approved_(address _owner, address spender, address token, uint256 amount)
+        external
+        view
+        returns (bool)
+    {
+        bool isApproved = isERC20Approved(_owner, spender, token, amount);
+        return (isApproved);
+    }
+}
