d414755d19dc5653310ae5c7e2495bc827f29438
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-05 12:32:21 -0400

    feat(`MagnetarHelper`): Add `roundUp` param to `getAmountForBorrowPart()` & `getBorrowPartForAmount()` [`86dtqt272`]

--- contracts/interfaces/periph/IMagnetarHelper.sol
+++ contracts/interfaces/periph/IMagnetarHelper.sol
@@ -15,9 +15,15 @@ import {ISingularity} from "tapioca-periph/interfaces/bar/ISingularity.sol";
 */
 
 interface IMagnetarHelper {
-    function getAmountForBorrowPart(address market, uint256 borrowPart) external view returns (uint256 amount);
+    function getAmountForBorrowPart(address market, uint256 borrowPart, bool roundUp)
+        external
+        view
+        returns (uint256 amount);
 
-    function getBorrowPartForAmount(address market, uint256 amount) external view returns (uint256 part);
+    function getBorrowPartForAmount(address market, uint256 amount, bool roundUp)
+        external
+        view
+        returns (uint256 part);
 
     function getFractionForAmount(ISingularity singularity, uint256 amount) external view returns (uint256 fraction);
 }
