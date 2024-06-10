f7c15b20e917a156a03da20a4808f49657e02446
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-09 20:12:42 -0400

    feat(`task`): Add new `exitPool__task`

---
+++ tasks/exec/lbp/exitPool__task.ts
@@ -0,0 +1,117 @@
+import { TapiocaMulticall } from '@typechain/index';
+import { loadLocalContract } from 'tapioca-sdk';
+import { TTapiocaDeployerVmPass } from 'tapioca-sdk/dist/ethers/hardhat/DeployerVM';
+import { WeightedPoolEncoder } from '@balancer-labs/balancer-js';
+import { DEPLOYMENT_NAMES, DEPLOY_CONFIG } from 'tasks/deploy/DEPLOY_CONFIG';
+import { deployLbp__getDeployments } from 'tasks/deploy/1-1-deployLbp';
+import { TTapiocaDeployTaskArgs } from '@tapioca-sdk/ethers/hardhat/DeployerVM';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { BalancerSDK } from '@balancer-labs/sdk';
+
+export const exitPool__task = async (
+    _taskArgs: TTapiocaDeployTaskArgs,
+    hre: HardhatRuntimeEnvironment,
+) => {
+    await hre.SDK.DeployerVM.tapiocaDeployTask(
+        _taskArgs,
+        { hre },
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        async () => {},
+        setup,
+    );
+};
+
+export async function setup(params: TTapiocaDeployerVmPass<object>) {
+    const { hre, VM, tapiocaMulticallAddr, taskArgs, chainInfo, isTestnet } =
+        params;
+    const { tag } = taskArgs;
+
+    console.log('[+] Exiting LBP');
+
+    const lbp = await hre.ethers.getContractAt(
+        'LiquidityBootstrappingPool',
+        loadLocalContract(
+            hre,
+            hre.SDK.chainInfo.chainId,
+            DEPLOYMENT_NAMES.TAP_USDC_LBP,
+            tag,
+        ).address,
+    );
+
+    const calls: TapiocaMulticall.CallStruct[] = [];
+
+    const vault = await hre.ethers.getContractAt(
+        'Vault',
+        loadLocalContract(
+            hre,
+            hre.SDK.chainInfo.chainId,
+            DEPLOYMENT_NAMES.LBP_VAULT,
+            tag,
+        ).address,
+    );
+
+    const { lTap } = deployLbp__getDeployments({ hre, tag });
+    const usdc = DEPLOY_CONFIG.MISC[chainInfo.chainId]!.USDC!;
+
+    const ltapContract = await hre.ethers.getContractAt(
+        '@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20',
+        lTap.address,
+    );
+    const usdContract = await hre.ethers.getContractAt(
+        '@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20',
+        usdc,
+    );
+
+    const ltapVaultBalance = await ltapContract.balanceOf(vault.address);
+    const usdcVaultBalance = await usdContract.balanceOf(vault.address);
+    const ownerPoolBalance = await lbp.balanceOf(tapiocaMulticallAddr);
+
+    const slippage = 1;
+    const ltapAmountWithdraw = ltapVaultBalance.sub(
+        ltapVaultBalance.div(100).mul(slippage),
+    );
+    const usdcAmountWithdraw = usdcVaultBalance.sub(
+        usdcVaultBalance.div(100).mul(slippage),
+    );
+
+    console.log('[+] LTAP amount to withdraw:', ltapAmountWithdraw.toString());
+    console.log('[+] USDC amount to withdraw:', usdcAmountWithdraw.toString());
+
+    calls.push(
+        {
+            target: lbp.address,
+            callData: lbp.interface.encodeFunctionData('setSwapEnabled', [
+                false,
+            ]),
+            allowFailure: false,
+        },
+        {
+            target: lbp.address,
+            callData: lbp.interface.encodeFunctionData('approve', [
+                vault.address,
+                hre.ethers.constants.MaxInt256,
+            ]),
+            allowFailure: false,
+        },
+        {
+            target: vault.address,
+            callData: vault.interface.encodeFunctionData('exitPool', [
+                await lbp.getPoolId(),
+                tapiocaMulticallAddr,
+                tapiocaMulticallAddr,
+                {
+                    assets: [lTap.address, usdc],
+                    minAmountsOut: [ltapAmountWithdraw, usdcAmountWithdraw],
+                    toInternalBalance: false,
+                    userData: WeightedPoolEncoder.exitBPTInForExactTokensOut(
+                        [ltapAmountWithdraw, usdcAmountWithdraw],
+                        ownerPoolBalance,
+                    ),
+                },
+            ]),
+            allowFailure: false,
+        },
+    );
+
+    await VM.executeMulticall(calls);
+}
--- tasks/scopes/lbpScope.ts
+++ tasks/scopes/lbpScope.ts
@@ -1,6 +1,7 @@
 import { scope } from 'hardhat/config';
 import { TAP_TASK } from 'tapioca-sdk';
 import { disableLbpSwaps__task } from 'tasks/exec/lbp/disableLbpSwaps__task';
+import { exitPool__task } from 'tasks/exec/lbp/exitPool__task';
 
 const lbpScope = scope('lbp', 'LBP setter tasks');
 
@@ -9,5 +10,13 @@ TAP_TASK(
         'disableSwaps',
         'Disable the LBP swaps',
         disableLbpSwaps__task,
+    ),
+);
+
+TAP_TASK(
+    lbpScope.task(
+        'exitPool',
+        'Exit and retrieve liquidity from the pool',
+        exitPool__task,
     ),
 );
