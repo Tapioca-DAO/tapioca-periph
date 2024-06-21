f9b1e9571bddf25139651d27b6ae425bfd241c68
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-21 15:14:57 -0400

    fix: oracle deploy config

--- tasks/deployBuilds/oracle/buildUsdoUsdcOracle.ts
+++ tasks/deployBuilds/oracle/buildUsdoUsdcOracle.ts
@@ -23,7 +23,7 @@ export const buildUsdoUsdcOracle = async (params: {
             _circuitUniIsMultiplied: [1], // Multiply/divide Uni
             _twapPeriod: isTestnet ? 1 : 3600, // TWAP, 1hr
             observationLength: isTestnet ? 1 : 10, // Observation length that each Uni pool should have
-            _uniFinalCurrency: 0, // Whether we need to use the last Chainlink oracle to convert to another
+            _uniFinalCurrency: 1, // Whether we need to use the last Chainlink oracle to convert to another
             _circuitChainlink: [
                 DEPLOY_CONFIG.POST_LBP[chainID]!.USDC_USD_CL_DATA_FEED_ADDRESS,
             ], // CL path
