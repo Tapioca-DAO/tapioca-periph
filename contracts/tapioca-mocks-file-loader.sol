2e75e6833ba32f5b3cc6513ae79cb429e6cc6117
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-08-28 10:59:54 -0400

    chore: put back tapioca mocks to load deployment files

---
+++ contracts/tapioca-mocks-file-loader.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+/// THIS FILE IS USED TO LOAD THE TAPIOCA MOCKS CONTRACTS
+/// Comment the imports for faster compilation
+
+import {ChainlinkFeedMock} from "tapioca-mocks/ChainlinkFeedMock.sol";
+import {ZeroXSwapperMock} from "tapioca-mocks/ZeroXSwapperMock.sol";
+import {OracleMock} from "tapioca-mocks/OracleMock.sol";
+import {ERC20Mock} from "tapioca-mocks/ERC20Mock.sol";
